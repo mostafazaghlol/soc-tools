261005d9216062f583522aec2a935e0c42489049
mostafa salah zaghloul <mostafas.zaghlol@gmail.com>
2021-09-18 08:21:50 +0200

    Create is_ip_vpn_or_not.py
    go to https://ipqualityscore.com sign up and get your free api key put it in var webapi and run the code you may want to run 
    > pip install requests

---
+++ is_ip_vpn_or_not.py
@@ -0,0 +1,31 @@
+import requests
+import ipaddress
+
+webapi="get your api key"
+site="https://ipqualityscore.com/api/json/ip/{}/{}?strictness=0&allow_public_access_points=true&fast=true&lighter_penalties=true&mobile=true"
+
+while(True):
+    ip = input("Enter the IP or nothing to stop the application !")
+    if not valid_ip(ip):
+        print("Enter a vaild ip !")
+        continue
+
+    if len(ip) == 0:
+        print("Thanks for using me :)")
+        break
+    
+    site=site.format(webapi,ip)
+    response=requests.get(site)
+    if response.json()['vpn'] :
+        print("{} is a VPN".format(ip))
+    else:
+        print("{} is Not a VPN".format(ip))    
+
+
+
+def valid_ip(address):
+    try: 
+        print (ipaddress.ip_address(address))
+        return True
+    except:
+        return False        
